e88a3d9d3819694847ebf044707309842cb63837
tsaiton123 <tsaiton123@gmail.com>
2024-09-09 17:03:25 +0800

    app

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-# from flask_ngrok import run_with_ngrok   # colab 使用，本機環境請刪除
-from urllib.parse import quote as url_quote
-from flask import Flask, request
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
-import json
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
-def linebot():
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    json_data = json.loads(body)
-    print(json_data)
+    app.logger.info("Request body: " + body)
     try:
-        line_bot_api = LineBotApi('你的 Channel access token')
-        handler = WebhookHandler('你的 LINE Channel secret')
-        signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
-        tk = json_data['events'][0]['replyToken']         # 取得 reply token
-        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
-        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
-        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
-    except:
-        print('error')
+    except InvalidSignatureError:
+        abort(400)
     return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
